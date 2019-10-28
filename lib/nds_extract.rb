0596b24b141e2e92e6785c3635371936a8a4f1f6
Alex Suthammanont <asuthamm@gmail.com>
2019-10-28 04:02:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,23 +7,7 @@ require 'pp'
 pp directors_database[1]
 # { directorOne => allTheMoneyTheyMade, ... }
 
-# def directors_totals(nds)
-#   result = {}
-#   #
-#   idx1 = 0
-#   while idx1 < directors_database.length
-#     sum=0
-#     idx2 = 0
-#     while idx2 < directors_database[idx1][:movies].length
-#       # p director_database[idx1][:movies][idx2][:worldwide_gross]
-#       sum += directors_database[idx1][:movies][idx2][:worldwide_gross]
-#       idx2 +=1
-#     end
-#     result[directors_database[idx1][:name]] = sum
-#     idx1 +=1
-#   end
-#   result
-# end
+
 
 
 # # Find a way to accumulate the :worldwide_grosses and return that Integer
