00e24d505879aaf1ad3149b35bc1778d496331c6
Alex Suthammanont <asuthamm@gmail.com>
2019-10-28 04:33:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,29 +5,29 @@ require 'pp'
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 
-# def directors_totals(nds)
-#   result = {}
-#   result = gross_for_director(nds)
+def directors_totals(nds)
+  result = {}
+  result = gross_for_director(nds)
 
-#   result 
-# end
-  # p directors_database
+  result 
+end
 
-# Find a way to accumulate the :worldwide_grosses and return that Integer
-# # using director_data as input
-# def gross_for_director(nds)
-#   result = {}
-#   idx1 = 0
-#   while idx1 < nds.length
-#     sum=0
-#     idx2 = 0
-#     while idx2 < nds[idx1][:movies].length
-#       # p director_database[idx1][:movies][idx2][:worldwide_gross]
-#       sum += nds[idx1][:movies][idx2][:worldwide_gross]
-#       idx2 +=1
-#     end
-#     result[nds[idx1][:name]] = sum
-#     idx1 +=1
-#   end
-#   result
-# end
+
+Find a way to accumulate the :worldwide_grosses and return that Integer
+# using director_data as input
+def gross_for_director(nds)
+  result = {}
+  idx1 = 0
+  while idx1 < nds.length
+    sum=0
+    idx2 = 0
+    while idx2 < nds[idx1][:movies].length
+      # p director_database[idx1][:movies][idx2][:worldwide_gross]
+      sum += nds[idx1][:movies][idx2][:worldwide_gross]
+      idx2 +=1
+    end
+    result[nds[idx1][:name]] = sum
+    idx1 +=1
+  end
+  result
+end
